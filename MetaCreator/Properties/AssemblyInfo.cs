a76c4462892b2f3ed759f97e592dad9340a6cefa
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2011-04-08 09:28:44 +0400

    Release 1.5

--- MetaCreator/Properties/AssemblyInfo.cs
+++ MetaCreator/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("MetaCreator")]
 [assembly: AssemblyProduct("MetaCreator")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyVersion("1.5.*")]

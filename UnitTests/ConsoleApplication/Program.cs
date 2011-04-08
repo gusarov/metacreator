6362f13aa488fd19aba8fa38153a53222948ab0a
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2011-04-08 08:57:56 +0400

    merge program.cs

--- UnitTests/ConsoleApplication/Program.cs
+++ UnitTests/ConsoleApplication/Program.cs
@@ -1,25 +1,10 @@
 ﻿using System;
 
-/* @ reference System.Xml.dll */
-/* @ using System.Xml */
-
-/* @ reference ..\..\ThirdParty\SomeThirdPartyProject.dll */
-/* @ using SomeThirdPartyProject */
-
-
 class q
 {
 	static void Main()
 	{
 		string macroResult = null;
-
-		var q = SomeThirdPartyProject.TestThirdPartyAPI.Test();
-		
-		/* !
-			var doc = new XmlDocument();
-
-			WriteLine("macroResult = \"" + TestThirdPartyAPI.Test()+"\";");
-		*/
 
 		Console.WriteLine(macroResult);
 	}

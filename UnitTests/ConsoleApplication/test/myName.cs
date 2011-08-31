f8998db8863e4f6c7e4e9760fea78b7519bf1ab5
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2011-08-31 22:08:17 +0400

    dll for testing

--- UnitTests/ConsoleApplication/test/myName.cs
+++ UnitTests/ConsoleApplication/test/myName.cs
@@ -244,6 +244,39 @@ namespace My
 	}
 
 
+	[Serializable]
+	struct MyTuple< T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13 >
+	{
+
+		public T1 Item1 { get; set; }
+
+		public T2 Item2 { get; set; }
+
+		public T3 Item3 { get; set; }
+
+		public T4 Item4 { get; set; }
+
+		public T5 Item5 { get; set; }
+
+		public T6 Item6 { get; set; }
+
+		public T7 Item7 { get; set; }
+
+		public T8 Item8 { get; set; }
+
+		public T9 Item9 { get; set; }
+
+		public T10 Item10 { get; set; }
+
+		public T11 Item11 { get; set; }
+
+		public T12 Item12 { get; set; }
+
+		public T13 Item13 { get; set; }
+ 
+	}
+
+
 #line 28 "C:\Users\x\Documents\Workspaces\metacreator\UnitTests\ConsoleApplication\MyTuple.tt"
 
 

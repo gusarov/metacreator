81910e20931bdae50fe2145a3896744c5e791d3b
DGusarov <DGusarov@DGusar01-DESK.paladyne.com>
2012-03-05 21:01:08 +0400

    separate skeleton

--- MetaCreator/Evaluation/Code1Builder.cs
+++ MetaCreator/Evaluation/Code1Builder.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 
 using MetaCreator.Utils;
 using Microsoft.Build.Framework;
+using MetaCreator.Properties;
 
 namespace MetaCreator.Evaluation
 {
@@ -131,80 +132,7 @@ namespace MetaCreator.Evaluation
 			_ctx.CSharpVersion = version;
 		}
 
-		const string _skeleton = @"// METACODE
-// This file is generated and compiled in-memory.
-// If there are any compilation errors - file can be saved to temporary path for working with IDE.
-
-#region Imports
-{0}
-#endregion
-
-public class Generator : MetaCreator.IGenerator, MetaCreator.IMetaWriter
-{{
-	readonly IMetaEngine Engine;
-
-	public Generator(IMetaEngine engine)
-	{{
-		Engine = engine;
-	}}
-
-	public string Run()
-	{{
-#region methodbody
-
-		{1}
-
-#endregion
-		return Result.ToString();
-	}}
-
-#region classbody
-
-{2}
-
-#endregion
-
-#region utils
-
-	public StringBuilder Result = new StringBuilder();
-
-	public void Write(string msg, params object[] args)
-	{{
-		Result.AppendFormat(msg, args);
-	}}
-
-	public void WriteLine(string msg, params object[] args)
-	{{
-		Result.AppendFormat(msg + Environment.NewLine, args);
-	}}
-
-	public void Write(string msg)
-	{{
-		Result.Append(msg);
-	}}
-
-	public void WriteLine(string msg)
-	{{
-		Result.AppendLine(msg);
-	}}
-
-	public void Write(object obj)
-	{{
-		Result.Append(obj == null ? string.Empty : obj.ToString());
-	}}
-
-	public void WriteLine(object obj)
-	{{
-		Result.AppendLine(obj == null ? string.Empty : obj.ToString());
-	}}
-
-	public void WriteLine()
-	{{
-		Result.AppendLine();
-	}}
-
-#endregion
-}}";
+		readonly static string _skeleton = Resources._GeneratorSkeleton;
 
 		static readonly string[] _defaultUsings = new[]
 		{
---
+++ MetaCreator/Evaluation/_GeneratorSkeleton.cs
@@ -0,0 +1,74 @@
+﻿// METACODE
+// This file is generated and compiled in-memory.
+// If there are any compilation errors - file can be saved to temporary path for working with IDE.
+
+#region Imports
+{0}
+#endregion
+
+public class Generator : MetaCreator.IGenerator, MetaCreator.IMetaWriter
+{{
+	readonly IMetaEngine Engine;
+
+	public Generator(IMetaEngine engine)
+	{{
+		Engine = engine;
+	}}
+
+	public string Run()
+	{{
+#region methodbody
+
+		{1}
+
+#endregion
+		return Result.ToString();
+	}}
+
+#region classbody
+
+{2}
+
+#endregion
+
+#region utils
+
+	public StringBuilder Result = new StringBuilder();
+
+	public void Write(string msg, params object[] args)
+	{{
+		Result.AppendFormat(msg, args);
+	}}
+
+	public void WriteLine(string msg, params object[] args)
+	{{
+		Result.AppendFormat(msg + Environment.NewLine, args);
+	}}
+
+	public void Write(string msg)
+	{{
+		Result.Append(msg);
+	}}
+
+	public void WriteLine(string msg)
+	{{
+		Result.AppendLine(msg);
+	}}
+
+	public void Write(object obj)
+	{{
+		Result.Append(obj == null ? string.Empty : obj.ToString());
+	}}
+
+	public void WriteLine(object obj)
+	{{
+		Result.AppendLine(obj == null ? string.Empty : obj.ToString());
+	}}
+
+	public void WriteLine()
+	{{
+		Result.AppendLine();
+	}}
+
+#endregion
+}}

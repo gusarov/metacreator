18ffaff09953a38db843a77f284c79789136bd4d
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2012-03-08 14:25:38 +0400

    mixin

---
+++ UnitTests/MetaCreator Acceptance/Multiple Inheritance/MixinMembers/_sample_mixin_events_exec.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+public class Base
+{
+}
+
+public class Test : Base /*= ": INotifyPropertyChanged " */
+{
+	/*# Mixin<Observable> */
+
+	int _testProperty;
+
+	public int TestProperty
+	{
+		get { return _testProperty; }
+		set
+		{
+			_testProperty = value;
+			OnPropertyChanged("TestProperty");
+		}
+	}
+
+}

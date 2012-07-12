54cba30ef3404b636ab45ccfa4e75e0ba45b2b92
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2012-07-12 13:36:29 +0400

    CSharpTypeIdentifier: allow omit generic type parameter name for typeof()

--- UnitTests/MetaCreator UnitTest/MixinExtensionTest.cs
+++ UnitTests/MetaCreator UnitTest/MixinExtensionTest.cs
@@ -1,4 +1,6 @@
-﻿using MetaCreator.Extensions;
+﻿using System.Collections.Generic;
+
+using MetaCreator.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
@@ -19,10 +21,10 @@ namespace MetaCreator_UnitTest
 		[TestMethod]
 		public void Should_return_type_name_with_regards_to_csharp_syntax()
 		{
-			Assert.AreEqual("MetaCreator_UnitTest.MixinExtensionTest", MixinExtension.CSharpTypeIdentifier(typeof(MixinExtensionTest)));
+			Assert.AreEqual("MetaCreator_UnitTest.MixinExtensionTest", typeof(MixinExtensionTest).CSharpTypeIdentifier());
 
-			Assert.AreEqual("string", MixinExtension.CSharpTypeIdentifier(typeof(System.String)));
-			Assert.AreEqual("int", MixinExtension.CSharpTypeIdentifier(typeof(int)));
+			Assert.AreEqual("string", typeof(System.String).CSharpTypeIdentifier());
+			Assert.AreEqual("int", typeof(int).CSharpTypeIdentifier());
 
 		}
 
@@ -38,6 +40,25 @@ namespace MetaCreator_UnitTest
 		{
 			Assert.AreEqual("MetaCreator_UnitTest.SampleGeneric<string, MetaCreator_UnitTest.SampleGeneric<long, short>>", MixinExtension.CSharpTypeIdentifier(typeof(SampleGeneric<System.String, SampleGeneric<long, short>>)));
 
+		}
+
+		[TestMethod]
+		public void Should_provide_generic_type_definition()
+		{
+			Assert.AreEqual("IDictionary<TKey, TValue>", typeof(IDictionary<,>).CSharpTypeIdentifier(null, "System.Collections.Generic"));
+			Assert.AreEqual("IList<T>", typeof(IList<>).CSharpTypeIdentifier(null, "System.Collections.Generic"));
+
+			var cfg = new SharpGenerator.TypeIdentifierConfig
+			{
+				UseNamedTypeParameters = false,
+				Imports = new[]
+				{
+					"System.Collections.Generic",
+				},
+			};
+
+			Assert.AreEqual("IDictionary<, >", typeof(IDictionary<,>).CSharpTypeIdentifier(cfg));
+			Assert.AreEqual("IList<>", typeof(IList<>).CSharpTypeIdentifier(cfg));
 		}
 	}
 }

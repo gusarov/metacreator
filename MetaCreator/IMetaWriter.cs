d6f64c683d01bbf1b651c01916fca0a40531d9bb
Dmitry Gusarov <Dmitry Gusarov@localhost>
2013-11-17 14:19:33 +0400

    TransactionalWriter

--- MetaCreator/IMetaWriter.cs
+++ MetaCreator/IMetaWriter.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace MetaCreator
 {
 	public interface IMetaWriter
@@ -9,5 +12,67 @@ namespace MetaCreator
 		void Write(object obj);
 		void WriteLine(object obj);
 		void WriteLine();
+	}
+
+	public static class TransactionalWriterExt
+	{
+		public static void Transactional(this IMetaWriter writer, Action<IMetaWriter> writingAction)
+		{
+			var collector = new TransactionalWriter();
+			writingAction(collector);
+			collector.Apply(writer);
+		}
+	}
+
+	public class TransactionalWriter : IMetaWriter
+	{
+		private readonly List<Action<IMetaWriter>> _actions = new List<Action<IMetaWriter>>();
+
+		internal TransactionalWriter()
+		{
+		}
+
+		public void Apply(IMetaWriter writer)
+		{
+			foreach (var action in _actions)
+			{
+				action(writer);
+			}
+		}
+
+		public void Write(string msg, params object[] args)
+		{
+			_actions.Add(w => w.Write(msg, args));
+		}
+
+		public void WriteLine(string msg, params object[] args)
+		{
+			_actions.Add(w => w.WriteLine(msg, args));
+		}
+
+		public void Write(string msg)
+		{
+			_actions.Add(w => w.Write(msg));
+		}
+
+		public void WriteLine(string msg)
+		{
+			_actions.Add(w => w.WriteLine(msg));
+		}
+
+		public void Write(object obj)
+		{
+			_actions.Add(w => w.Write(obj));
+		}
+
+		public void WriteLine(object obj)
+		{
+			_actions.Add(w => w.WriteLine(obj));
+		}
+
+		public void WriteLine()
+		{
+			_actions.Add(w => w.WriteLine());
+		}
 	}
 }

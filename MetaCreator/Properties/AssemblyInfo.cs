fc769de9b834e3e2e8c600b2fab4334ff8afc4cf
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2014-11-05 16:46:43 +0300

    +multipass for 3.5 +auto level raise +1.7 ver

--- MetaCreator/Properties/AssemblyInfo.cs
+++ MetaCreator/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("MetaCreator")]
 [assembly: AssemblyProduct("MetaCreator")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
--- UnitTests/ConsoleApplication/SameAssemblyGeneration1.cs
+++ UnitTests/ConsoleApplication/SameAssemblyGeneration1.cs
@@ -6,8 +6,6 @@ using System.Text;
 namespace ConsoleApplication
 {
 	/*@ fileInProject */
-	/*@ metaLevel 1 */
-	/*@ requiresLevel 255 */
 
 	public static class SameAssemblyGeneration1
 	{
